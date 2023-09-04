dae897a8f093a7d0760706ce1b4b24dcb4180955
Jorge Agulló <agullojorge@gmail.com>
2023-09-04 23:24:34 +0200

    add challenge 2

---
+++ app/src/main/java/com/softyorch/retosdeprogramacion/challenges/Challenge02.kt
@@ -0,0 +1,41 @@
+package com.softyorch.retosdeprogramacion.challenges
+
+import java.util.Locale
+
+/*
+ * Escribe un programa que reciba un texto y transforme lenguaje natural a
+ * "lenguaje hacker" (conocido realmente como "leet" o "1337"). Este lenguaje
+ *  se caracteriza por sustituir caracteres alfanuméricos.
+ * - Utiliza esta tabla (https://www.gamehouse.com/blog/leet-speak-cheat-sheet/)
+ *   con el alfabeto y los números en "leet".
+ *   (Usa la primera opción de cada transformación. Por ejemplo "4" para la "a")
+ */
+
+fun theHackerLanguage(text: String) {
+    var textResult = ""
+    text.forEach { char ->
+        textResult += if (char.isDigit()) {
+            searchNumber(char)
+        } else if (char.isLetter()) {
+            searchChar(char)
+        } else {
+            char
+        }
+    }
+    printSolution("Texto original: $text")
+    printSolution("Transformación a leet: $textResult")
+}
+
+fun searchNumber(char: Char): String = leetNumbers.elementAt(numbers.indexOf(char.toString().uppercase(Locale.ROOT)))
+
+fun searchChar(char: Char): String = leetAlphabet.elementAt(alphabet.indexOf(char.lowercase(Locale.ROOT)))
+
+fun printSolution(value: String) = println(value)
+
+val alphabet = arrayListOf("a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z", "á", "é", "í", "ó", "ú")
+
+val leetAlphabet = arrayListOf("4", "I3", "[", ")", "3", "|=", "&", "#", "1", ",_l", ">|", "1", """/\/\""", "^/", "0", "|*", "(_,)", "I2", "5", "7", "(_)", """\/""", """\/\/""", "><", "j", "2", "4", "3", "1", "0", "(_)")
+
+val numbers = arrayListOf("1", "2", "3", "4", "5", "6", "7", "8", "9", "0")
+
+val leetNumbers = arrayListOf("L", "R", "E", "A", "S", "b", "T", "B", "g", "o")
---
+++ app/src/main/java/com/softyorch/retosdeprogramacion/kotlin/Main.kt
@@ -0,0 +1,23 @@
+package com.softyorch.retosdeprogramacion.kotlin
+
+import com.softyorch.retosdeprogramacion.challenges.leapYears
+import com.softyorch.retosdeprogramacion.challenges.theHackerLanguage
+
+fun main() {
+
+    println("Porfavor, introduce el número de prueba: ")
+    when (readlnOrNull()?.toInt() ?: 0) {
+        0 -> leapYears()
+        1 -> theHackerLanguage("Código leet. Hola, me llamo Jorge y nací en 1982. Soy desarrollador de software.")
+        else -> {
+            println("No se ha encontrado la prueba")
+        }
+    }
+
+
+    //Años bisiestos
+    //leapYears()
+
+    //Alfabeto leet
+
+}

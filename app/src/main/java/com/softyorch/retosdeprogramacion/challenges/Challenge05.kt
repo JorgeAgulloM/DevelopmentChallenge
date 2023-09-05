c27b6a4acf64189a4ba30ce699b398a4374f7984
Jorge Agulló <agullojorge@gmail.com>
2023-09-05 08:18:25 +0200

    add challenge 5

---
+++ app/src/main/java/com/softyorch/retosdeprogramacion/challenges/Challenge05.kt
@@ -0,0 +1,42 @@
+package com.softyorch.retosdeprogramacion.challenges
+
+/*
+ * Escribe un programa que, dado un número, compruebe y muestre si es primo,
+ * fibonacci y par.
+ * Ejemplos:
+ * - Con el número 2, nos dirá: "2 es primo, fibonacci y es par"
+ * - Con el número 7, nos dirá: "7 es primo, no es fibonacci y es impar"
+ */
+
+
+fun whatIsThisNumber() {
+
+    val numSelect = (0..100).random()
+    val isPrime = if (isPrime(numSelect)) "$numSelect es primo" else "$numSelect no es primo"
+    val isFibonacci = if (isFibonacci(numSelect)) "fibonacci" else "no es fibonacci"
+    val isPair = if (isPair(numSelect)) "es par" else "es impar"
+    println("$isPrime, $isFibonacci y $isPair")
+
+}
+
+fun isPrime(num: Int): Boolean {
+    for (i in 2..< num) {
+        if (num % i == 0) {
+            return false
+        }
+    }
+    return true
+}
+
+fun isFibonacci(num: Int): Boolean {
+    val fibonacci = arrayListOf(1, 2)
+    if (num == 1 || num == 2) return true
+    for (i in 1..num) {
+        fibonacci.add(fibonacci.last() + fibonacci.elementAt(i - 1))
+        if (fibonacci.last() == num) return true
+        if (fibonacci.last() > num) return false
+    }
+    return false
+}
+
+fun isPair(num: Int): Boolean = num % 2 == 0
--- app/src/main/java/com/softyorch/retosdeprogramacion/kotlin/Main.kt
+++ app/src/main/java/com/softyorch/retosdeprogramacion/kotlin/Main.kt
@@ -4,6 +4,7 @@ import com.softyorch.retosdeprogramacion.challenges.leapYears
 import com.softyorch.retosdeprogramacion.challenges.passGenerator
 import com.softyorch.retosdeprogramacion.challenges.tennisMatch
 import com.softyorch.retosdeprogramacion.challenges.theHackerLanguage
+import com.softyorch.retosdeprogramacion.challenges.whatIsThisNumber
 
 fun main() {
 
@@ -13,6 +14,7 @@ fun main() {
         1 -> theHackerLanguage("Código leet. Hola, me llamo Jorge y nací en 1982. Soy desarrollador de software.")
         2 -> tennisMatch()
         3 -> passGenerator()
+        4 -> whatIsThisNumber()
         else -> {
             println("No se ha encontrado la prueba")
         }
